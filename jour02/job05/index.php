79a35a98488e1cd6fd8383466e717dcc8fe19ca9
Marc Keil <marc.keil@laplateforme.io>
2022-03-15 17:21:39 +0100

    fin job 05 jpp

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,16 +1,15 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JS</title>
-    <script type="text/javascript" src="script.js"></script>
+    <title>Job 05</title>
     <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
 <body>
-    
-
-<footer></footer>
+    <footer id="footer">
+    </footer>
 </body>
 </html>
--- jour02/job05/script.js
+++ jour02/job05/script.js
@@ -1,8 +1,26 @@
+
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function loaded() {
 
+var pos = document.getElementById('footer');
+
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight; // Resultat du scrolltop
+
+window.addEventListener('scroll', function(){
+    var progress = (document.documentElement.scrollTop / height) * 100;
+
+    pos.style.width = progress + "%";
+
+    if (progress <= 35 ){
+        pos.style.background = "green";
+    }
+    else if (progress > 35  && progress <= 70 ) {
+        pos.style.background = "red";
+    }
+    else if (progress > 70  && progress <= 100 ) {
+        pos.style.background = "orange";
+    }
     
-
-
+})
 })
