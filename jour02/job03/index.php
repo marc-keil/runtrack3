b34f4d2767329ece63e663aab2dcd37c022bbecb
Marc Keil <marc.keil@laplateforme.io>
2022-03-15 13:35:52 +0100

    job 3 finiti pipi

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JS</title>
-    <script type="text/javascript" src="script.js"></script>
+    <title>Job 03</title>
+    <script src="script.js"></script>
 </head>
+
 <body>
-    
+    <main>
+    <p id="compteur">0</p>
+    <button id="button">Click</button>
+    </main>
+
 </body>
+
 </html>
--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -1,1 +1,19 @@
-window.alert("Hello Javascript!");
+/* Créez un button ayant comme id “button”. Créez une balise <p> ayant
+comme id “compteur” et contenant “0”. Ce contenu doit évoluer
+proportionnellement au nombre d'événements click reçu par le bouton.
+Vous ne devez pas utiliser “onclick()” dans votre html. La fonction
+permettant d’effectuer la modification doit s'appeler “addone()”.*/
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function loaded() {
+    var bouton = document.getElementById('button');
+    var p = document.getElementById('compteur');
+
+    function addone(){
+        var nombreclick = compteur.innerHTML;
+        nombreclick++;
+        compteur.innerHTML = nombreclick;
+    }
+
+    bouton.addEventListener('click', addone)
+});
