4c22badcf8e8fa6380de693ffc610f78f88cd5c5
Marc Keil <marc.keil@laplateforme.io>
2022-03-16 17:38:34 +0100

    le 02 est fini

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -8,23 +8,17 @@
 </head>
 
 <body>
-    <section id="tableaux">
-        <div id="melangees">
-
-
-        </div>
-        <button id="button">Mélanger</button>
-        <div id="rangees">
-            <img id="id1" src="arc1.png"></img>
-            <img id="id2" src="arc2.png"></img>
-            <img id="id3" src="arc3.png"></img>
-            <img id="id4" src="arc4.png"></img>
-            <img id="id5" src="arc5.png"></img>
-            <img id="id6" src="arc6.png"></img>
-        </div>
-    </section>
+<div id="melangees"></div>
+<div id="rangees">
+    <img id="arc1" class="arc" src="arc1.png">
+    <img id="arc2" class="arc" src="arc2.png">
+    <img id="arc3" class="arc" src="arc3.png">
+    <img id="arc4" class="arc" src="arc4.png">
+    <img id="arc5" class="arc" src="arc5.png">
+    <img id="arc6" class="arc" src="arc6.png">
+</div>
+<button id="button">Mélanger</button>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="script.js"></script>
 </body>
-
 </html>
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="script.js"></script>
--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -10,20 +10,43 @@
 // dessous. Si l’arc-en ciel est bien reconstitué, le message “Vous avez
 // gagné” s’affiche en vert. Sinon, le message “Vous avez perdu” s’affiche en
 // rouge.
-$("#button").click(function()
-{
-    for(var i = 0;i<=50;i++)
-    {
 
-        var alea = Math.floor((Math.random() * 6) + 1);
-        $("#id" + alea).appendTo($("#melangees"));
-        console.log(i);
+// Melange un tableau
 
+let shuffle = function (){
+    let array = [0,1,2,3,4,5]
+    array = array.sort(() => Math.random() - 0.5) // Array sort qui permet de ranger un tableau en random pour rendre tout aléatoire
+    return array
+}
+
+// Melange les images dans la div melangees
+$('#button').on('click',function (){
+    let allPics = $('#rangees img'); // Récupere tout les images avec IMG de la div rangée
+    let shuffleArray = shuffle() // Utilise la fonction shuffle du dessus
+    for (let i = 0 ; i < shuffleArray.length; i++){
+       let pic = allPics[shuffleArray[i]];
+            $('#melangees').append(pic) // Envoie les images dans melangees
     }
+})
+var pics = $('.arc') // Je récupere toute les images avec la classe arc
 
-});
+// Quand on clique sur une des image 
 
-$("img").click(function()
-{   
-$('#id').appendTo("#rangees");
-});
+pics.on('click',function (){
+    // IMPORTANT attr permet de récup les attribut ici This fait reference à l'image cliqué
+    //On récupere donc l'Id de l'element de classe .arc cliqué
+    let picID = $(this).attr("id")
+    //Je récupere le format et je l'append dans la div rangée
+    $('#rangees').append($('#'+picID))
+    // Control final
+    if ($('#rangees > img').length === 6) {
+        if ($('#rangees img:first-child').attr("id") === 'arc1' && $('#rangees img:nth-child(2)').attr("id") === 'arc2' && $('#rangees img:nth-child(3)').attr("id") === 'arc3' && $('#rangees img:nth-child(4)').attr("id") === 'arc4' && $('#rangees img:nth-child(5)').attr("id") === 'arc5' && $('#rangees img:nth-child(6)').attr("id") === 'arc6') {
+            $("#rangees").after("<p>Vous avez gagné!</p>").addClass('green')
+            $('p').css('color','green');
+
+        } else {
+            $("#rangees").after("<p>Vous avez perdu!</p>")
+            $('p').css('color','red');
+        }
+    }
+})
