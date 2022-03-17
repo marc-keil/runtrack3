92fedd80f9d5ba9aacd2e72bd628a0476ea0cfc1
GrosGras <marc.keil@laplateforme.io>
2022-03-17 12:28:16 +0100

    ajout d'un com

--- jour04/job01/script.js
+++ jour04/job01/script.js
@@ -15,7 +15,7 @@ bouton.on('click', () => {
     fetch('expression.txt')
         .then((reponse) => reponse.text())    // On prend la réponse et on la fait devenir du texte
         .then((data) => {                     // On récupere les données qu'on met dans la data
-            $('p').append(data+"<br>")
+            $('p').append(data+"<br>") // On peut remplacer le "append" par "text", qui empeche les repetitions
         })
         .catch((error) => console.log(`%cERREUR: ${error.message}`, "color: red;"))
     ;
