4b0ac8a478ae28154f3d0dc9dba8dac55745477c
Marc Keil <marc.keil@laplateforme.io>
2022-03-21 13:56:17 +0100

    fin job 1 et 2

---
+++ jour06/job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <link rel="stylesheet" type="text/css" href="style3.css">
+    <link rel="stylesheet" type="text/css" href="style4.css">
+    <title>Job01</title>
+</head>
+<body>
+    
+</body>
+</html>
